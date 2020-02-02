91b791b43e103f5484acee66c84ba1137a908b6a
Danny Moulton <ddmoulton@gmail.com>
2020-02-02 17:53:29 -0500

    added ComponentTypes models

---
+++ core/src/main/java/io/kestros/cms/foundation/componenttypes/ComponentType.java
@@ -0,0 +1,342 @@
+package io.kestros.cms.foundation.componenttypes;
+
+import static io.kestros.cms.foundation.utils.ComponentTypeUtils.getAllComponentTypes;
+import static io.kestros.cms.foundation.utils.ComponentTypeUtils.getComponentTypeGroups;
+import static io.kestros.cms.foundation.utils.DesignUtils.getAllUiFrameworks;
+import static io.kestros.commons.structuredslingmodels.utils.SlingModelUtils.getChildAsBaseResource;
+import static io.kestros.commons.structuredslingmodels.utils.SlingModelUtils.getChildrenAsBaseResource;
+import static io.kestros.commons.structuredslingmodels.utils.SlingModelUtils.getResourceAsType;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.kestros.cms.foundation.componenttypes.frameworkview.ComponentUiFrameworkView;
+import io.kestros.cms.foundation.design.uiframework.UiFramework;
+import io.kestros.cms.foundation.exceptions.InvalidCommonUiFrameworkException;
+import io.kestros.cms.foundation.exceptions.InvalidComponentTypeException;
+import io.kestros.cms.foundation.exceptions.InvalidComponentUiFrameworkViewException;
+import io.kestros.cms.foundation.exceptions.InvalidScriptException;
+import io.kestros.cms.foundation.utils.DesignUtils;
+import io.kestros.commons.structuredslingmodels.BaseResource;
+import io.kestros.commons.structuredslingmodels.annotation.Property;
+import io.kestros.commons.structuredslingmodels.annotation.StructuredModel;
+import io.kestros.commons.structuredslingmodels.exceptions.ChildResourceNotFoundException;
+import io.kestros.commons.structuredslingmodels.exceptions.InvalidResourceTypeException;
+import io.kestros.commons.structuredslingmodels.exceptions.ModelAdaptionException;
+import io.kestros.commons.structuredslingmodels.exceptions.ResourceNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Exporter;
+import org.apache.sling.models.annotations.Model;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Model for resource that will be implemented by Components via sling:resourceType property.
+ */
+@StructuredModel(validationService = ComponentTypeValidationService.class,
+                 docPaths = {
+                     "/content/guide-articles/kestros/components/creating-new-component-types",
+                     "/content/guide-articles/kestros/components/implementing-ui-framework-views",
+                     "/content/guide-articles/kestros/components/defining-content-areas",
+                     "/content/guide-articles/kestros-cms/foundation/creating-component-variations",
+                     "/content/guide-articles/kestros-cms/foundation/grouping-components"})
+@Model(adaptables = Resource.class,
+       resourceType = "kes:ComponentType")
+@Exporter(name = "jackson",
+          selector = "component-type",
+          extensions = "json")
+public class ComponentType extends BaseResource {
+
+  private static final Logger LOG = LoggerFactory.getLogger(ComponentType.class);
+
+  public static final String COMMON_UI_FRAMEWORK_VIEW_NAME = "common";
+  public static final String PN_COMPONENT_GROUP = "componentGroup";
+  public static final String GROUP_PREFIX = "group:";
+  public static final String PN_EXCLUDED_UI_FRAMEWORKS = "excludedUiFrameworks";
+  public static final String PN_EXCLUDED_COMPONENT_TYPES = "excludedComponentTypes";
+  public static final String PN_ALLOWED_COMPONENT_TYPES = "allowedComponentTypes";
+
+  /**
+   * Group the current ComponentType belongs to. Set by the componentGroup property.
+   *
+   * @return Group the current ComponentType belongs to.
+   */
+  public String getComponentGroup() {
+    return getProperties().get(PN_COMPONENT_GROUP, StringUtils.EMPTY);
+  }
+
+  /**
+   * The sling:resourceSuperType as a ComponentType.
+   *
+   * @return The sling:resourceSuperType as a ComponentType.
+   * @throws InvalidComponentTypeException ComponentSuperType could not be found, or failed
+   *     adaption.
+   */
+  @JsonIgnore
+  public ComponentType getComponentSuperType() throws InvalidComponentTypeException {
+    try {
+      // TODO what to do when /apps is passed here, but does not exist, and /libs does.
+      return getResourceAsType(getResourceSuperType(), getResourceResolver(), ComponentType.class);
+    } catch (final InvalidResourceTypeException exception) {
+      LOG.debug("Unable to retrieve superType of {}: {}", getPath(), exception.getMessage());
+      throw new InvalidComponentTypeException(getResourceSuperType(),
+          "Invalid resourceType, must be 'kes:ComponentType'.");
+    } catch (final ResourceNotFoundException exception) {
+      LOG.debug("Unable to retrieve superType of {}: {}", getPath(), exception.getMessage());
+      throw new InvalidComponentTypeException(getResourceSuperType(), "Resource not found.");
+    }
+  }
+
+  /**
+   * The common ComponentUiFrameworkView, to be shown when no other valid views are found.
+   *
+   * @return The common ComponentUiFrameworkView,to be shown when no other valid views are found.
+   * @throws InvalidCommonUiFrameworkException Common ComponentUiFramework could not be found,
+   *     or failed adaption.
+   */
+  @JsonIgnore
+  @Nonnull
+  public ComponentUiFrameworkView getCommonUiFrameworkView()
+      throws InvalidCommonUiFrameworkException {
+
+    try {
+      final ComponentUiFrameworkView commonUiFrameworkView = getChildAsBaseResource(
+          COMMON_UI_FRAMEWORK_VIEW_NAME, this).getResource().adaptTo(
+          ComponentUiFrameworkView.class);
+      if (commonUiFrameworkView != null) {
+        return commonUiFrameworkView;
+      }
+    } catch (final Exception exception) {
+      LOG.debug("Unable to retrieve 'common' ComponentUiFrameworkView of {}: {}", getPath(),
+          exception.getMessage());
+    }
+    // TODO clean up when logged.
+    throw new InvalidCommonUiFrameworkException(getPath());
+  }
+
+  @Property(description = "")
+  boolean isBypassUiFrameworks() {
+    return getProperties().get("bypassUiFrameworks", Boolean.FALSE);
+  }
+
+  /**
+   * All UI Framework view that belong to the current ComponentType.
+   *
+   * @return All UI Framework view that belong to the current ComponentType.
+   */
+  @JsonIgnore
+  @Nonnull
+  public List<ComponentUiFrameworkView> getUiFrameworkViews() {
+    final List<ComponentUiFrameworkView> uiFrameworkViews = new ArrayList<>();
+
+    for (final BaseResource uiFrameworkViewResource : getChildrenAsBaseResource(this)) {
+      if (!getExcludedUiFrameworkPaths().contains(uiFrameworkViewResource.getName())) {
+        final ComponentUiFrameworkView uiFrameworkView
+            = uiFrameworkViewResource.getResource().adaptTo(ComponentUiFrameworkView.class);
+        uiFrameworkViews.add(uiFrameworkView);
+      }
+    }
+
+    return uiFrameworkViews;
+  }
+
+  /**
+   * Filters ComponentTypes within a list of ComponentTypeGroups to retrieve only the allowed
+   * components.
+   *
+   * @return Filters ComponentTypes within a list of ComponentTypeGroups to retrieve only the
+   *     allowed components.
+   */
+  @Nonnull
+  public List<ComponentTypeGroup> getAllowedComponentTypeGroups() {
+    return getComponentTypeGroups(
+        getAllComponentTypes(true, false, isAllowLibsCommonsComponents(), getResourceResolver()),
+        getAllowedComponentTypePaths(), getExcludedComponentTypePaths(),
+        getAllowedComponentTypeGroupNames(), getExcludedComponentTypeGroups());
+  }
+
+  @Nonnull
+  public Boolean isAllowLibsCommonsComponents() {
+    return getProperty("allowLibsCommons", Boolean.TRUE);
+  }
+
+  /**
+   * The ComponentUiFrameworkView for the specified UiFramework, or the Common
+   * ComponentUiFrameworkView.
+   *
+   * @param uiFramework The UiFramework to lookup a view for.
+   * @return The ComponentUiFrameworkView for the specified UiFramework, or the Common
+   *     CommponetUiFrameworkView.
+   * @throws InvalidComponentUiFrameworkViewException ComponentUiFramework for the specified
+   *     UiFramework was not found, or failed adaption.
+   */
+  @Nonnull
+  public ComponentUiFrameworkView getComponentUiFrameworkView(
+      @Nonnull final UiFramework uiFramework) throws InvalidComponentUiFrameworkViewException {
+    try {
+      return DesignUtils.getComponentUiFrameworkView(uiFramework.getFrameworkCode(), this);
+
+    } catch (final ChildResourceNotFoundException exception) {
+      LOG.debug("{} view was not found for {}. Returning common view.",
+          uiFramework.getFrameworkCode(), getPath());
+    }
+    try {
+      return DesignUtils.getComponentUiFrameworkView(uiFramework.getName(), this);
+    } catch (final ChildResourceNotFoundException exception) {
+      LOG.debug("{} view was not found for {}. Returning common view.", uiFramework.getName(),
+          getPath());
+    }
+    try {
+      return getCommonUiFrameworkView();
+    } catch (final InvalidCommonUiFrameworkException exception) {
+      LOG.debug("Unable to retrieve ComponentUiFrameworkView for ComponentType {}", getPath());
+    }
+    throw new InvalidComponentUiFrameworkViewException(getPath(), uiFramework);
+  }
+
+  /**
+   * Gets the specified HtmlFile script for the specified script name and UiFramework.
+   *
+   * @param scriptName script to lookup.
+   * @param uiFramework UiFramework used to determine which ComponentUiFrameworkView to find the
+   *     script in.
+   * @return the specified HtmlFile script for the specified script name and UiFramework.
+   * @throws InvalidScriptException Script was not found, or failed adaption.
+   */
+  @Nonnull
+  public HtmlFile getScript(@Nonnull final String scriptName,
+      @Nullable final UiFramework uiFramework)
+      throws InvalidScriptException, InvalidCommonUiFrameworkException {
+    if (uiFramework != null) {
+      try {
+        return getComponentUiFrameworkView(uiFramework).getUiFrameworkViewScript(scriptName);
+      } catch (final ModelAdaptionException exception) {
+        LOG.trace("Script {} not found for {} while using UiFramework {}. Looking to superType",
+            scriptName, getPath(), uiFramework.getPath());
+      }
+      try {
+        return getScriptFromSuperType(scriptName, uiFramework);
+      } catch (final InvalidComponentTypeException exception) {
+        LOG.debug(exception.getMessage());
+      }
+    }
+    return getCommonUiFrameworkView().getUiFrameworkViewScript(scriptName);
+  }
+
+  /**
+   * Font Awesome Icon class.
+   *
+   * @return Font Awesome Icon class.
+   */
+  public String getFontAwesomeIcon() {
+    String fontAwesomeIcon = getProperty("fontAwesomeIcon", StringUtils.EMPTY);
+    if (StringUtils.isBlank(fontAwesomeIcon)) {
+      try {
+        fontAwesomeIcon = getComponentSuperType().getFontAwesomeIcon();
+      } catch (final InvalidComponentTypeException exception) {
+        LOG.debug("Unable to find FontAwesomeIcon for {} due to InvalidComponentType on {}.",
+            getPath(), getResourceType());
+      }
+    }
+    if (StringUtils.isBlank(fontAwesomeIcon)) {
+      fontAwesomeIcon = "fa fa-cube";
+    }
+    return fontAwesomeIcon;
+  }
+
+  List<String> getAllowedComponentTypeGroupNames() {
+    final List<String> allowedComponentTypeGroups = new ArrayList<>();
+
+    for (final String type : getAllowedComponentTypes()) {
+      if (type.startsWith(GROUP_PREFIX)) {
+        final String groupName = type.replaceFirst(GROUP_PREFIX, StringUtils.EMPTY);
+        allowedComponentTypeGroups.add(groupName);
+      }
+    }
+
+    return allowedComponentTypeGroups;
+  }
+
+  List<String> getAllowedComponentTypePaths() {
+    final List<String> allowedComponentTypePaths = new ArrayList<>();
+
+    for (final String type : getAllowedComponentTypes()) {
+      if (!type.startsWith(GROUP_PREFIX)) {
+        allowedComponentTypePaths.add(type);
+      }
+    }
+
+    return allowedComponentTypePaths;
+  }
+
+  String[] getAllowedComponentTypes() {
+    return getProperty(PN_ALLOWED_COMPONENT_TYPES, new String[]{});
+  }
+
+  List<String> getExcludedComponentTypeGroups() {
+    final List<String> excludedComponentTypeGroups = new ArrayList<>();
+
+    for (final String type : getExcludedComponentTypes()) {
+      if (type.startsWith(GROUP_PREFIX)) {
+        final String groupName = type.replaceFirst(GROUP_PREFIX, StringUtils.EMPTY);
+        excludedComponentTypeGroups.add(groupName);
+      }
+    }
+
+    return excludedComponentTypeGroups;
+  }
+
+  List<String> getExcludedComponentTypePaths() {
+    final List<String> excludedComponentTypePaths = new ArrayList<>();
+
+    for (final String type : getExcludedComponentTypes()) {
+      if (!type.startsWith(GROUP_PREFIX)) {
+        excludedComponentTypePaths.add(type);
+      }
+    }
+
+    return excludedComponentTypePaths;
+  }
+
+  List<String> getMissingUiFrameworkCodes() {
+    final List<String> missingUiFrameworkCodes = new ArrayList<>();
+    for (final UiFramework uiFramework : getAllUiFrameworks(getResourceResolver(), true, true)) {
+      boolean found = false;
+      ComponentUiFrameworkView componentUiFrameworkView = null;
+      try {
+        componentUiFrameworkView = getComponentUiFrameworkView(uiFramework);
+      } catch (final InvalidComponentUiFrameworkViewException e) {
+        // TODO log.
+      }
+      if (componentUiFrameworkView != null && !componentUiFrameworkView.getName().equals(
+          COMMON_UI_FRAMEWORK_VIEW_NAME)) {
+        found = true;
+      }
+
+      if (!found && !getExcludedUiFrameworkPaths().contains(uiFramework.getFrameworkCode())
+          && !getExcludedUiFrameworkPaths().contains(uiFramework.getName())) {
+        missingUiFrameworkCodes.add(uiFramework.getFrameworkCode());
+      }
+    }
+
+    return missingUiFrameworkCodes;
+  }
+
+  private List<String> getExcludedUiFrameworkPaths() {
+    return Arrays.asList(getProperties().get(PN_EXCLUDED_UI_FRAMEWORKS, new String[]{}));
+  }
+
+  private List<String> getExcludedComponentTypes() {
+    return Arrays.asList(getProperty(PN_EXCLUDED_COMPONENT_TYPES, new String[]{}));
+  }
+
+  private HtmlFile getScriptFromSuperType(final String scriptName, final UiFramework uiFramework)
+      throws InvalidComponentTypeException, InvalidScriptException,
+             InvalidCommonUiFrameworkException {
+    return this.getComponentSuperType().getScript(scriptName, uiFramework);
+  }
+}
---
+++ core/src/main/java/io/kestros/cms/foundation/componenttypes/ComponentTypeGroup.java
@@ -0,0 +1,62 @@
+package io.kestros.cms.foundation.componenttypes;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+import javax.annotation.Nonnull;
+
+public class ComponentTypeGroup {
+
+  private String title;
+  private final List<ComponentType> componentTypes = new ArrayList<>();
+
+  public ComponentTypeGroup() {
+    this.title = "No Group";
+  }
+
+  public void setTitle(@Nonnull final String title) {
+    this.title = title;
+  }
+
+  @Nonnull
+  public String getTitle() {
+    return title;
+  }
+
+  public void addComponentType(@Nonnull final ComponentType componentType) {
+    this.componentTypes.add(componentType);
+  }
+
+  /**
+   * Removes the specified ComponentType from the current ComponentTypeGroup.
+   *
+   * @param path ComponentType to remove.
+   */
+  public void removeComponentType(@Nonnull final String path) {
+    this.componentTypes.removeIf(componentType -> path.equals(componentType.getPath()));
+  }
+
+  public void removeComponentType(@Nonnull final ComponentType componentType) {
+    this.removeComponentType(componentType.getPath());
+  }
+
+  @Nonnull
+  public List<ComponentType> getComponentTypes() {
+    componentTypes.sort(new ComponentTypeSorter());
+    return componentTypes;
+  }
+
+  private static class ComponentTypeSorter implements Comparator<ComponentType>, Serializable {
+
+    private static final long serialVersionUID = -365044729256904870L;
+
+    @Override
+    public int compare(final ComponentType o1, final ComponentType o2) {
+      return o1.getTitle().compareTo(o2.getTitle());
+    }
+  }
+
+
+}
