1c388380e7655bf126ef242360e162d00a5c3c67
Danny Moulton <ddmoulton@gmail.com>
2020-02-02 14:08:59 -0500

    added DesignConstants

---
+++ core/src/main/java/io/kestros/cms/foundation/design/DesignConstants.java
@@ -0,0 +1,23 @@
+package io.kestros.cms.foundation.design;
+
+/**
+ * Constants for Design level Kestros values.
+ */
+public class DesignConstants {
+
+  public static final String THEME_PRIMARY_TYPE = "kes:Theme";
+  public static final String VENDOR_LIBRARIES_ETC_ROOT_PATH = "/etc/vendor-libraries";
+  public static final String VENDOR_LIBRARIES_LIBS_ROOT_PATH = "/libs/kestros/vendor-libraries";
+  public static final String UI_FRAMEWORKS_ETC_ROOT_PATH = "/etc/ui-frameworks";
+  public static final String UI_FRAMEWORKS_LIBS_ROOT_PATH = "/libs/kestros/ui-frameworks";
+  public static final String NN_THEMES = "themes";
+  public static final String NN_VARIATIONS = "variations";
+  public static final String PN_THEME_PATH = "kes:theme";
+  public static final String PN_UI_FRAMEWORK_CODE = "kes:uiFrameworkCode";
+  public static final String PN_VENDOR_LIBRARIES = "kes:vendorLibraries";
+
+  private DesignConstants() {
+    // EMPTY
+  }
+
+}
