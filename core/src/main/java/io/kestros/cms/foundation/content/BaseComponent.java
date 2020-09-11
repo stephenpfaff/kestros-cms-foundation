efc22d47f94715fe96dbc282f6f5a69227060eef
Danny Moulton <danny.moulton@kestros.io>
2020-09-11 12:12:25 -0400

    updated title and description property descriptions

--- core/src/main/java/io/kestros/cms/foundation/content/BaseComponent.java
+++ core/src/main/java/io/kestros/cms/foundation/content/BaseComponent.java
@@ -362,13 +362,18 @@ public class BaseComponent extends BaseResource {
   }
 
   @Override
-  @KestrosProperty(description = "")
+  @KestrosProperty(description = "Component title. Generally used for administrative purposes.",
+                   configurable = true,
+                   jcrPropertyName = "jcr:title")
   public String getTitle() {
     return super.getTitle();
   }
 
   @Override
-  @KestrosProperty(description = "")
+  @KestrosProperty(description = "Component description. Generally used for administrative "
+                                 + "purposes.",
+                   configurable = true,
+                   jcrPropertyName = "jcr:description")
   public String getDescription() {
     return super.getDescription();
   }
